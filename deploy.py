838545618e125e70223f67885c61082d3f07129a
Zachary Weeden <zweeden@datto.com>
2017-10-05 13:23:46 -0400

    Reworked structure

--- deploy.py
+++ deploy.py
@@ -4,116 +4,36 @@ Authors: Gaetano Carlucci, Giuseppe Cofano
 Modified: Zachary Weeden
 """
 import os
-import pathlib
-import subprocess
-import multiprocessing
-
-
-class CPULoad:
-    """
-    Constructs necessary python files (if needed) on instantiation and runs the package with parameters specified.
-    For each core a new process is opened to mitigate issues and locks similar to piped invocation in the original script.
-    """
-
-    def __init__(self):
-
-        self.file_path = os.path.expanduser('~')
-        if pathlib.Path(f'{self.file_path}/cpu_stress.py').is_file() is False:
-            with open(f'{self.file_path}/cpu_stress.py', 'w') as f:
-                f.write(cpu_load_main)
-
-        if pathlib.Path(f'{self.file_path}/monitor.py').is_file() is False:
-            with open(f'{self.file_path}/monitor.py', 'w') as f:
-                f.write(monitor)
-
-        if pathlib.Path(f'{self.file_path}/controller.py').is_file() is False:
-            with open(f'{self.file_path}/controller.py', 'w') as f:
-                f.write(controller)
-
-        if pathlib.Path(f'{self.file_path}/closed_loop_actuator.py').is_file() is False:
-            with open(f'{self.file_path}/closed_loop_actuator.py', 'w') as f:
-                f.write(actuator)
-
-    def load(self, core_num, load_percent):
-        """
-        :param core_num: int - enum of cpu core
-        :param load_percent: float - percent of CPU loaded (0.0 - 100.0)
-        :return:
-        """
-        load_percent = float(load_percent/100)
-        subprocess.Popen(
-            ['python', f'{self.file_path}/cpu_stress.py', '--cpu-core', str(core_num), '--cpu-load', str(load_percent)])
-
-
-cpu_load_main = """
-import sys
-import argparse
-
-from monitor import MonitorThread
-from controller import ControllerThread
-from closed_loop_actuator import closedLoopActuator
-
-parser = argparse.ArgumentParser(description='Load designated CPU cores to certain percent')
-parser.add_argument('--cpu-core', type=int, default=0, help='Core to be run on')
-parser.add_argument('--cpu-load', type=float, default=.2, help='CPU load - percent used eg. .20')
-options = parser.parse_args()
-
-cpu_config = vars(options)
-
-monitor = MonitorThread([cpu_config['cpu_core']], 0.1)
-monitor.start()
-
-control = ControllerThread(0.1)
-control.start()
-control.setCpuTarget(cpu_config['cpu_load']) # float eg. .1 =10% 1.0=100%
-
-# control thread, monitor thread, desired cpu usage
-actuator = closedLoopActuator(control, monitor, cpu_config['cpu_load'])
-actuator.run()
-
-monitor.running = 0
-control.running = 0
-monitor.join()
-control.join()
-"""
-
-monitor = """
-import os, psutil
+import time
+import psutil
 import threading
 import multiprocessing
 
-class MonitorThread(threading.Thread):
 
-    def __init__(self, cpu_cores, interval):
-        self.sampling_interval = interval  # sample time interval
-        self.sample = 0.5  # cpu load measurement sample
-        self.cpu = 0.5  # cpu load filtered
-        self.running = 1  # thread status
-        self.alpha = 1  # filter coefficient
-        self.sleepTimeTarget = 0.03
-        self.sleepTime = 0.03
-        self.cpuTarget = 0.5
-        self.cpu_cores = cpu_cores  # array to support on process on multiple cores
-        super(MonitorThread, self).__init__()
+class ClosedLoopActuator():
+    def __init__(self, controller, monitor, target):
+        self.controller = controller
+        self.monitor = monitor
+        self.target = target
+        self.controller.setCpu(self.monitor.getCpuLoad())
+        self.period = 0.05  # actuation period  in seconds
 
-    def getCpuLoad(self):
-        return self.cpu
+    def generate_load(self, sleep_time):
+        interval = time.time() + self.period - sleep_time
+        # generates some getCpuLoad for interval seconds
+        while (time.time() < interval):
+            pr = 213123  # generates some load
+            pr * pr
+            pr = pr + 1
+
+        time.sleep(sleep_time)
 
     def run(self):
-        p = psutil.Process(os.getpid())
-        if len(self.cpu_cores) > 0:
-            p.cpu_affinity(self.cpu_cores)
-        else:
-            p.cpu_affinity([coreNum for coreNum in range(multiprocessing.cpu_count())])
-        while self.running:
-            self.sample = p.cpu_percent(self.sampling_interval)
-            # first order filter on the measurement samples
-            self.cpu = self.alpha * self.sample + (1 - self.alpha) * self.cpu
-"""
+        while 1:
+            self.controller.setCpu(self.monitor.getCpuLoad())
+            sleep_time = self.controller.getSleepTime()
+            self.generate_load(sleep_time)
 
-controller = """
-import threading
-import time
 
 class ControllerThread(threading.Thread):
     def __init__(self, interval, ki=None, kp=None):
@@ -122,7 +42,7 @@ class ControllerThread(threading.Thread):
         self.period = 0.1  # actuation period  in seconds
         self.sleepTime = 0.02  # this is controller output: determines the sleep time to achieve the requested CPU load
         self.alpha = 0.2  # filter coefficient
-        self.CT = 0.20  # target CPU load should be provided as input 
+        self.CT = 0.20  # target CPU load should be provided as input
         self.cpu = 0  # current CPU load returned from the Monitor thread
         self.cpuPeriod = 0.03
         if ki is None:
@@ -131,7 +51,7 @@ class ControllerThread(threading.Thread):
             self.kp = 0.02  # proportional constant of th PI regulator
         self.int_err = 0  # integral error
         self.last_ts = time.time()  # last sampled time
-        super(ControllerThread, self).__init__()
+        super().__init__()
 
     def getSleepTime(self):
         return self.sleepTime
@@ -163,39 +83,71 @@ class ControllerThread(threading.Thread):
                 self.cpuPeriod = self.period
                 self.int_err = self.int_err - self.err * samp_int
             self.sleepTime = self.period - self.cpuPeriod
-"""
 
-actuator = """
-import time
 
-class closedLoopActuator():
+class MonitorThread(threading.Thread):
+    def __init__(self, cpu_cores, interval):
+        self.sampling_interval = interval  # sample time interval
+        self.sample = 0.5  # cpu load measurement sample
+        self.cpu = 0.5  # cpu load filtered
+        self.running = 1  # thread status
+        self.alpha = 1  # filter coefficient
+        self.sleepTimeTarget = 0.03
+        self.sleepTime = 0.03
+        self.cpuTarget = 0.5
+        self.cpu_cores = cpu_cores  # array to support on process on multiple cores
+        super().__init__()
 
-    def __init__(self, controller, monitor, target):
-        self.controller = controller
-        self.monitor = monitor
-        self.target = target
-        self.controller.setCpu(self.monitor.getCpuLoad())
-        self.period = 0.05  # actuation period  in seconds
-        self.last_plot_time = time.time()
-        self.start_time = time.time()
-
-    def generate_load(self, sleep_time):
-        interval = time.time() + self.period - sleep_time
-        # generates some getCpuLoad for interval seconds
-        while (time.time() < interval):
-            pr = 213123 # generates some load
-            pr * pr
-            pr = pr + 1
-
-        time.sleep(sleep_time)
+    def getCpuLoad(self):
+        return self.cpu
 
     def run(self):
-        while 1:
-            self.controller.setCpu(self.monitor.getCpuLoad())
-            sleep_time = self.controller.getSleepTime()
-            self.generate_load(sleep_time)
-"""
+        p = psutil.Process(os.getpid())
+        if len(self.cpu_cores) > 0:
+            p.cpu_affinity(self.cpu_cores)
+        else:
+            p.cpu_affinity([coreNum for coreNum in range(multiprocessing.cpu_count())])
+        while self.running:
+            self.sample = p.cpu_percent(self.sampling_interval)
+            # first order filter on the measurement samples
+            self.cpu = self.alpha * self.sample + (1 - self.alpha) * self.cpu
 
-stressor = CPULoad()
-for core in range(multiprocessing.cpu_count()):
-    stressor.load(core, .33)
+
+def cpu_stress(cpu_core, cpu_load):
+    monitor = MonitorThread([cpu_core], 0.1)
+    monitor.start()
+
+    control = ControllerThread(0.1)
+    control.start()
+    control.setCpuTarget(cpu_load)
+
+    # control thread, monitor thread, desired cpu usage
+    actuator = ClosedLoopActuator(control, monitor, cpu_load)
+    actuator.run()
+
+    monitor.running = 0
+    control.running = 0
+    monitor.join()
+    control.join()
+
+
+class CPULoad:
+    """CPU load process generator for each core"""
+    def __init__(self, load):
+        """
+        :param load: the CPU load desired
+        """
+        try:
+            self.load_percent = float(load)
+        except ValueError:
+            raise TypeError('CPU load average must be a number')
+        if self.load_percent < 0.0 or self.load_percent > 100.0:
+            raise ValueError('CPU load average must be within 0.0 and 100.0')
+
+    def load(self):
+        load_percent = float(self.load_percent / 100)
+        for core in range(multiprocessing.cpu_count()):
+            core_process = multiprocessing.Process(target=cpu_stress, args=(core, load_percent,))
+            core_process.start()
+
+CPULoad(33.3).load()
